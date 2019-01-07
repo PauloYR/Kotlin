4a44d70d5ae97972e3f8300b5b35174c245d89a4
pauloyr <pauloyr@outlook.como>
2019-01-07 11:22:04 -0300

    07/01/2019

---
+++ OrientadoObjeto/src/Carro.kt
@@ -0,0 +1,6 @@
+class Carro{
+    var cor: String = ""
+    var ano: Int = 2019
+    var modelo: String = "Camaro"
+
+}
---
+++ OrientadoObjeto/src/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var carro = Carro()
+    carro.ano = 2017
+    carro.cor = "Rosa"
+    carro.modelo = "Fox"
+    println("Ano do carro ${carro.ano}")
+    println("Cor do carro ${carro.cor}")
+    println("Modelo do Carro ${carro.modelo}")
+}
+
